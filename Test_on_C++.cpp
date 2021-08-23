bc542ab7acb968497259471338a5c3c897f1505d
zigmobil <russian.fantomas@yandex.ru>
2021-08-23 20:14:50 +0400

    Русский комментарий

---
+++ Test_on_C++.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <cwchar>
+#include <string>
+int main()
+{
+	system("chcp 1251"); // переходим в консоли на русский язык
+	system("cls");		 // очистка экрана
+
+	std::string name{ "Золото\n" };
+	printf("%s", &name);
+	
+	char alphabet[27]; 
+		for (int i = 0; i < 26; i++) {
+			alphabet[i] = i + 97; 
+		}
+	alphabet[26] = 0; 
+		printf("%s\n", alphabet); 
+		for (int i = 0; i < 26; i++) {
+			alphabet[i] = i + 65; 
+		}
+	printf("%s", alphabet); 
+}
+
